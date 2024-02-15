cc2752d5351ec7c90e36bb1ede2b38aab50e0c4e
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-15 16:24:22 +0500

    controll added

---
+++ MarketplaceSolution/Online.Marketplace/Controllers/MarketController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Online.Marketplace.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MarketController : ControllerBase
+    {
+    }
+}
