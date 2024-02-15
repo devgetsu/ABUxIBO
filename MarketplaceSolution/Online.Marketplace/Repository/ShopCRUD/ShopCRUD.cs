095609ae49818533098c2607cf4dd0558c9b356c
Abu_Programmiy <abdukholiq23@gmail.com>
2024-02-15 18:34:21 +0500

    ShopCRUD ham tugadi

--- MarketplaceSolution/Online.Marketplace/Repository/ShopCRUD/ShopCRUD.cs
+++ MarketplaceSolution/Online.Marketplace/Repository/ShopCRUD/ShopCRUD.cs
@@ -1,32 +1,114 @@
-﻿using Online.Marketplace.Models;
+﻿using Dapper;
+using Npgsql;
+using Online.Marketplace.Entities.DTOs;
+using Online.Marketplace.Models;
 
 namespace Online.Marketplace.Repository.ShopCRUD
 {
     public class ShopCRUD : IShopCRUD
     {
-        public string Create(Shops shop)
+        public IConfiguration _config;
+        public ShopCRUD(IConfiguration config)
         {
-            throw new NotImplementedException();
+            _config = config;
+        }
+        public string Create(ShopsDTO shop)
+        {
+            try
+            {
+                using (NpgsqlConnection con = new NpgsqlConnection(_config.GetConnectionString("Postgres")))
+                {
+                    string query = "insert into Shops(shop_name) " +
+                        "values(@shop_name=name);";
+
+                    ShopsDTO? parameters = new ShopsDTO
+                    {
+                        Shop_Name = shop.Shop_Name,
+                    };
+
+                    con.Execute(query, parameters);
+
+                    return "Succesfully";
+                }
+            }
+            catch
+            {
+                return "ERROR";
+            }
         }
 
         public string DeleteByID(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (NpgsqlConnection con = new NpgsqlConnection(_config.GetConnectionString("Postgres")))
+                {
+                    string query = "delete from shops where id = @aydi";
+
+                    con.Execute(query, new { aydi = id });
+
+                    return "Succesfully";
+                }
+            }
+            catch
+            {
+                return "ERROR";
+            }
         }
 
         public IEnumerable<Shops> GetAll()
         {
-            throw new NotImplementedException();
+            using (NpgsqlConnection con = new NpgsqlConnection(_config.GetConnectionString("Postgres")))
+            {
+                string query = "select * from shops";
+
+                IEnumerable<Shops>? responce = con.Query<Shops>(query);
+
+                return responce;
+            }
         }
 
         public Shops GetByID(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                using (NpgsqlConnection con = new NpgsqlConnection(_config.GetConnectionString("Postgres")))
+                {
+                    string query = "select * from shops where id = @aydi";
+
+                    List<Shops>? responce = con.Query<Shops>(query, new { aydi = id }).ToList();
+
+                    return responce[0];
+                }
+            }
+            catch
+            {
+                return new Shops() { };
+            }
         }
 
-        public Shops Update(int id, Shops product)
+        public string Update(int id, ShopsDTO shops)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (NpgsqlConnection con = new NpgsqlConnection(_config.GetConnectionString("Postgres")))
+                {
+                    string query = "update shops set shop_name=@shop_name where id = @aydi";
+
+                    con.Execute(query, new
+                    {
+                        shop_name=shops.Shop_Name,
+                        aydi = id
+                    });
+
+                    return "Succesfully";
+                }
+            }
+            catch
+            {
+                return "ERROR";
+            }
         }
     }
 }
